8938dfd38dd4146359518d00a6b63c9cc7499977
Alex Fischman <alexdfischman@gmail.com>
2024-06-08 22:26:25 -0700

    Add files via upload

---
+++ configuration.nix
@@ -0,0 +1,92 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+
+  # Locale
+  time.timeZone = "America/Los_Angeles";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  services.xserver = {
+    # Window
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+    windowManager.awesome.enable = true;
+
+    # Keymap
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
+  };
+
+  # Printing
+  services.printing.enable = true;
+
+  # Sound
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # User
+  users.users.alex = {
+    isNormalUser = true;
+    description = "Alex";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Packages
+  nixpkgs.config = {
+    allowUnfree = true;
+    permittedInsecurePackages = [
+      "openssl-1.1.1w" # sublime4 dependency
+    ];
+  };
+  environment.systemPackages = with pkgs; [
+    awesome
+    discord
+    gcc
+    gh
+    git
+    google-chrome
+    inkscape
+    pavucontrol
+    rustup
+    spotify
+    sublime4
+    texlive.combined.scheme-full
+    zoom-us
+  ];
+
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05";
+}
